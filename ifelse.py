59c9cefb8fb0637734748a7ec647a0c2b15df0d9
PeterMwitiDev <mwitipet@gmal.com>
2024-09-05 16:03:50 +0300

    added the ifelse file

---
+++ ifelse.py
@@ -0,0 +1,26 @@
+#if statement
+
+num = 30
+if num>=20 :
+    print("The number is greater tha 20")
+#if else
+
+weekday = True
+if weekday:
+    print("wake up early")
+else:
+    print("sleep")
+
+#if elif else
+
+grade = 54
+if grade >=90:
+    print("A")
+elif grade >= 80:
+    print("B")
+elif grade >=70:
+    print("C")
+elif grade >=60:
+    print("D")
+else:
+    print("fail")
